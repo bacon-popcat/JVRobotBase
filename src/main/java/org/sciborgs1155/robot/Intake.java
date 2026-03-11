0fe838e4b65f495fd0f2cb7e5542f8f3612b5ffc
Oscar Liu <oscarpro6908@gmail.com>
2026-03-10 23:40:17 -0400

    Need to add checks on motors

--- src/main/java/org/sciborgs1155/robot/Intake.java
+++ src/main/java/org/sciborgs1155/robot/Intake.java
@@ -1,19 +1,28 @@
 package org.sciborgs1155.robot;
 
+import com.revrobotics.RelativeEncoder;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Intake extends SubsystemBase {
+    // Booleans
     static boolean isDown = false;
     static boolean intakeMotorActivated = false;
     static boolean isInAutonomous = false;
+
+    // Constants
     static double SlapDownMotorPower = 0.9;
     static double IntakeMotorPower = 0.5;
+    static double DownPosition = 10; // *Note*: This is just a placeholder value that needs to be tested
+    static double UpPosition = 10;
+
+    // Motor variables
     private final static SparkMax Top_IntakeMotor = new SparkMax(Ports.Intake.TOP_INTAKE, MotorType.kBrushless);
     private final static SparkMax Bottom_IntakeMotor = new SparkMax(Ports.Intake.BOTTOM_INTAKE, MotorType.kBrushless);
     private final static SparkMax SlapDown_Motor = new SparkMax(Ports.Intake.SLAPDOWN, MotorType.kBrushless);
+    private final RelativeEncoder SlapdownEnconder = SlapDown_Motor.getEncoder();
     /* when you want to begin outtaking (like if it gets jammed or something)
      reverse the motor powers to reverse the spin of the motors */ 
 
