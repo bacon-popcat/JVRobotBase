ac34f76f003552b62d4d0a3d62ec2321f894bed6
Oscar Liu <oscarpro6908@gmail.com>
2026-03-10 23:07:37 -0400

    40% done intake

--- src/main/java/org/sciborgs1155/robot/Intake.java
+++ src/main/java/org/sciborgs1155/robot/Intake.java
@@ -6,46 +6,54 @@ import com.revrobotics.spark.SparkLowLevel.MotorType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Intake extends SubsystemBase {
-    
     static boolean isDown = false;
     static boolean intakeMotorActivated = false;
     static boolean isInAutonomous = false;
     static double SlapDownMotorPower = 0.9;
     static double IntakeMotorPower = 0.5;
-    private final SparkMax topIntakeMotor = new SparkMax(Ports, null)
+    private final static SparkMax Top_IntakeMotor = new SparkMax(Ports.Intake.TOP_INTAKE, MotorType.kBrushless);
+    private final static SparkMax Bottom_IntakeMotor = new SparkMax(Ports.Intake.BOTTOM_INTAKE, MotorType.kBrushless);
+    private final static SparkMax SlapDown_Motor = new SparkMax(Ports.Intake.SLAPDOWN, MotorType.kBrushless);
     /* when you want to begin outtaking (like if it gets jammed or something)
      reverse the motor powers to reverse the spin of the motors */ 
 
     public static void beginIntakeMotor() {
         // The sign may vary based on design
-        topIntakeMotor.setPower(IntakeMotorPower);
-        bottomIntakeMotor.setPower(-IntakeMotorPower);
+        Top_IntakeMotor.set(IntakeMotorPower);
+        Bottom_IntakeMotor.set(-IntakeMotorPower);
     }
 
     public static void beginOuttakeMotor() {
         // The sign may vary based on design
-        topIntakeMotor.setPower(-IntakeMotorPower);
-        bottomIntakeMotor.setPower(IntakeMotorPower);
+        Top_IntakeMotor.set(-IntakeMotorPower);
+        Bottom_IntakeMotor.set(IntakeMotorPower);
     }
 
     public static void stopMotor() {
-        topIntakeMotor.setPower(0.0);
-        bottomIntakeMotor.setPower(0.0);
+        Top_IntakeMotor.set(0.0);
+        Bottom_IntakeMotor.set(0.0);
     }
 
     public static void TriggerSlapdown() {
         if (isDown) {
             // if slap-down is down then gears will activate backwards
-            slapDownMotor.setPower(-SlapDownMotorPower);
+            SlapDown_Motor.set(-SlapDownMotorPower);
+
+            // Need to add code here to detect when motor reaches fully up
+
             isDown = false;
         } else {
             // If the intake isn’t already down, then activate motors to turn it down.
-            slapDownMotor.setPower(SlapDownMotorPower);
+
+            SlapDown_Motor.set(SlapDownMotorPower);
+
+            // Need to add code here to detect when motor reaches fully down
+
             isDown = true;
         } 
     }
 
-    public static void TriggerOutTake() {
+    public static void TriggerIntake() {
          if (intakeMotorActivated) {
             stopMotor();
          } else {
