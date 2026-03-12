b1679f3ba30f4fd379071770d3a4d10b5a5ef375
Oscar Liu <oscarpro6908@gmail.com>
2026-03-12 16:56:34 -0400

    Co-authored-by: Felix Phu <FelixPhu@users.noreply.github.com>

--- src/main/java/org/sciborgs1155/robot/Intake.java
+++ src/main/java/org/sciborgs1155/robot/Intake.java
@@ -1,10 +1,24 @@
 package org.sciborgs1155.robot;
 
+
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkBase.ControlType;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.ClosedLoopConfig;
+import com.revrobotics.spark.config.SparkMaxConfig;
 
+
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+
+/* TO DO
+- Auto intake system for autonomous phase (sensors detect game piece then activate the intake)
+- Link button to do stuff (after cad is finished)
+- 
+*/
+
 
 public class Intake extends SubsystemBase {
     // Booleans
@@ -12,62 +26,97 @@ public class Intake extends SubsystemBase {
     static boolean intakeMotorActivated = false;
     static boolean isInAutonomous = false;
 
+
     // Constants
     static double SlapDownMotorPower = 0.9;
     static double IntakeMotorPower = 0.5;
     static double DownPosition = 10; // *Note*: This is just a placeholder value that needs to be tested
-    static double UpPosition = 10;
-
+    static double UpPosition = 0;
+//
     // Motor variables
     private final static SparkMax Top_IntakeMotor = new SparkMax(Ports.Intake.TOP_INTAKE, MotorType.kBrushless);
     private final static SparkMax Bottom_IntakeMotor = new SparkMax(Ports.Intake.BOTTOM_INTAKE, MotorType.kBrushless);
     private final static SparkMax SlapDown_Motor = new SparkMax(Ports.Intake.SLAPDOWN, MotorType.kBrushless);
-    private final RelativeEncoder SlapdownEnconder = SlapDown_Motor.getEncoder();
     /* when you want to begin outtaking (like if it gets jammed or something)
-     reverse the motor powers to reverse the spin of the motors */ 
-
+     reverse the motor powers to reverse the spin of the motors */
+   
     public static void beginIntakeMotor() {
         // The sign may vary based on design
         Top_IntakeMotor.set(IntakeMotorPower);
         Bottom_IntakeMotor.set(-IntakeMotorPower);
     }
 
-    public static void beginOuttakeMotor() {
+
+    public static void reverseIntakeMotor() {
         // The sign may vary based on design
         Top_IntakeMotor.set(-IntakeMotorPower);
         Bottom_IntakeMotor.set(IntakeMotorPower);
     }
 
-    public static void stopMotor() {
-        Top_IntakeMotor.set(0.0);
-        Bottom_IntakeMotor.set(0.0);
+
+    public static void stopIntakeMotor() {
+        Top_IntakeMotor.stopMotor();
+        Bottom_IntakeMotor.stopMotor();
     }
+
 
     public static void TriggerSlapdown() {
         if (isDown) {
             // if slap-down is down then gears will activate backwards
-            SlapDown_Motor.set(-SlapDownMotorPower);
+           
+            SlapDown_Motor.getClosedLoopController()
+                .setReference(UpPosition, ControlType.kPosition);
 
-            // Need to add code here to detect when motor reaches fully up
 
             isDown = false;
         } else {
             // If the intake isn’t already down, then activate motors to turn it down.
 
-            SlapDown_Motor.set(SlapDownMotorPower);
 
-            // Need to add code here to detect when motor reaches fully down
+            SlapDown_Motor.getClosedLoopController()
+                .setReference(DownPosition, ControlType.kPosition);
+
 
             isDown = true;
-        } 
+        }
     }
 
-    public static void TriggerIntake() {
+
+    public static void TriggerIntake() { //togggle intake, if its
          if (intakeMotorActivated) {
-            stopMotor();
+            stopIntakeMotor();
          } else {
-            beginOuttakeMotor();
+            beginIntakeMotor();
          }
     }
-}
 
+
+    public Intake() {
+        // PID stuff
+        SparkMaxConfig config = new SparkMaxConfig();
+
+
+        ClosedLoopConfig pid = config.closedLoop;
+        pid.p(0.1); // Proportional: How strong the motor reacts to error (Error is the difference between current and target position).
+                    // Higher P = fast response
+                    // Very high P = May cause shaking/oscillation (Basically, do not set it high)
+       
+        pid.i(0); /*  Integral: Controls how fast the motor corrects its position;
+                        This is because friction and gravity cause the motor to have trouble rotate to the correct orientation
+                    */
+
+                    // Examples:
+                    // 0 = no correction
+                    // Very low I = correction may fall short
+                    // High I = correction may overshoot/oscillate
+
+        pid.d(0.01); //Derivative: Controls how fast the motor position changes. Slows down near target position
+
+                    // Examples:
+                    // With D = More damping (decrease overtime)
+                    // High D = May have sluggish/slow movement
+
+        // May need to change persistMode based on preference
+        SlapDown_Motor.configure(config, null, null);
+    };
+}
